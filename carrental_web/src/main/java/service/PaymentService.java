57547ad6d88a7dca737d09fefd15290eda35fa37
SoosSarolta <44021500+SoosSarolta@users.noreply.github.com>
2018-10-26 14:18:25 +0200

    részfeladat 1.1

--- carrental_web/src/main/java/service/PaymentService.java
+++ carrental_web/src/main/java/service/PaymentService.java
@@ -1,5 +1,10 @@
 package service;
 
+import model.Payment;
+import model.Booking;
+
 public class PaymentService {
-    
+    // kliens elküldi a pénzt - nála a payment 0 lesz
+    // admin befogadja a pénzt - nála a payment az adott összeg lesz
+    // booking végleges lesz - elmentésre kerül
 }
