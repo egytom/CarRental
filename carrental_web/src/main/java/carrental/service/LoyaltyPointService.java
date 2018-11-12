f8059de31c461eab59c41480910761c543e4cc0a
sombali <sombali97@gmail.com>
2018-11-12 17:56:19 +0100

    LoyaltyService TDD  2.- Balázs

--- carrental_web/src/main/java/carrental/service/LoyaltyPointService.java
+++ carrental_web/src/main/java/carrental/service/LoyaltyPointService.java
@@ -1,8 +1,27 @@
 package carrental.service;
 
+import carrental.repository.CarRepository;
+import carrental.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.beans.Transient;
 
 @Service
 public class LoyaltyPointService {
 
+    @Autowired
+    ClientRepository clientRepository;
+
+    @Autowired
+    CarRepository carRepository;
+
+    @Autowired
+    DiscountService discountService;
+
+    @Transactional
+    public void addLoyaltyPoints() {
+
+    }
 }
--- carrental_web/src/test/java/carrental/service/LoyaltyPointServiceTDD.java
+++ carrental_web/src/test/java/carrental/service/LoyaltyPointServiceTDD.java
@@ -1,11 +1,25 @@
 package carrental.service;
 
+import carrental.model.Booking;
+import carrental.model.Car;
+import carrental.model.Client;
+import carrental.model.Payment;
+import carrental.repository.CarRepository;
+import carrental.repository.ClientRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,8 +27,61 @@ import org.springframework.transaction.annotation.Transactional;
 @AutoConfigureTestDatabase
 public class LoyaltyPointServiceTDD {
 
-    @Test
-    public void testLoyaltyPointService() {
+    @Autowired
+    LoyaltyPointService loyaltyPointService;
 
+    @Autowired
+    DiscountService discountService;
+
+    @Autowired
+    ClientRepository clientRepository;
+
+    @Autowired
+    CarRepository carRepository;
+
+    private Date fromDate;
+    private Date toDate;
+    private Client client;
+    private Car car;
+    private Payment payment;
+    private Booking booking;
+
+    @Test
+    public void testInitialization() {
+        //ARRANGE
+        initialize();
+
+        //ACT
+        loyaltyPointService.addLoyaltyPoints();
+
+        //ASSERT
+        assertThat(client.getLoyaltyPoint(), equalTo(0));
+
+
+    }
+
+    private void initialize() {
+        Calendar cal = setCalendar();
+        fromDate = cal.getTime();
+
+        cal.set(Calendar.DAY_OF_MONTH, 22);
+        toDate = cal.getTime();
+
+        car = new Car();
+
+        payment = new Payment(1, 10000);
+        client = new Client(1, "Balázs", "email@email.hu", "+36");
+
+        booking = new Booking(1, fromDate, toDate, car, payment, client);
+
+        client.addBooking(booking);
+    }
+
+    private Calendar setCalendar() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, 2018);
+        cal.set(Calendar.MONTH, Calendar.NOVEMBER);
+        cal.set(Calendar.DAY_OF_MONTH, 10);
+        return cal;
     }
 }
