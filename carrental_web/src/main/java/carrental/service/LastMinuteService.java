2ad765d9fa553fd0fad8754849f9e6cf4c06487e
SoosSarolta <44021500+SoosSarolta@users.noreply.github.com>
2018-11-12 20:02:00 +0100

    TDD 2. - Soós Sarolta

--- carrental_web/src/main/java/carrental/service/LastMinuteService.java
+++ carrental_web/src/main/java/carrental/service/LastMinuteService.java
@@ -1,5 +1,7 @@
 package carrental.service;
 
+import carrental.model.Booking;
+import carrental.model.Client;
 import carrental.repository.BookingRepository;
 import carrental.repository.CarRepository;
 import carrental.repository.ClientRepository;
@@ -7,6 +9,8 @@ import carrental.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class LastMinuteService {
@@ -24,7 +28,13 @@ public class LastMinuteService {
     BookingRepository bookingRepository;
 
     @Transactional
-    public void EcoLastMinuteDiscount() {
+    public void EcoLastMinuteDiscount(Client client) {
+
+        List<Client> alreadyClient = clientRepository.findByName(client.getName());
+        List<Booking> bookings;
+
+        if (alreadyClient.isEmpty() == false)
+            bookings = bookingRepository.findByClient(client);
 
     }
 }
--- carrental_web/src/test/java/carrental/service/LastMinuteServiceTDD.java
+++ carrental_web/src/test/java/carrental/service/LastMinuteServiceTDD.java
@@ -1,5 +1,6 @@
 package carrental.service;
 
+import carrental.model.*;
 import carrental.repository.BookingRepository;
 import carrental.repository.CarRepository;
 import carrental.repository.ClientRepository;
@@ -9,13 +10,18 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
 @AutoConfigureTestDatabase
+@ActiveProfiles("test")
 public class LastMinuteServiceTDD {
 
     @Autowired
@@ -36,5 +42,29 @@ public class LastMinuteServiceTDD {
     @Test
     public void testEcoLastMinuteDiscount() throws Exception {
 
+        //ARRANGE
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date fromDate = new Date();
+        Date toDate = sdf.parse("2019-01-01");
+
+        Client client = new Client("Teszt Kliens", "teszt@kliens.hu");
+        Car car = new Car(1, "Passat", new Type(5, Type.GearType.auto, Type.FuelType.electric), new Category("Volkswagen"));
+        Payment payment = new Payment(1, 5000);
+
+        car = carRepository.save(car);
+        client = clientRepository.save(client);
+        payment = paymentRepository.save(payment);
+
+        Booking booking = new Booking(1, fromDate, toDate, car, payment, client);
+
+        booking = bookingRepository.save(booking);
+        client.addBooking(booking);
+
+        //ACT
+        lastMinuteService.EcoLastMinuteDiscount(client);
+
+        //ASSERT
+        client = clientRepository.findByName("Teszt Kliens").get(0);
+        booking = bookingRepository.findByClient(client).get(0);
     }
 }
