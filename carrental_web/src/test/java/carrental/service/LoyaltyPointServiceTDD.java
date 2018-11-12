421c23c9e61bc6bf2f82ceafe0bf4ed7d4c3ce9c
sombali <sombali97@gmail.com>
2018-11-12 17:02:44 +0100

    LoyaltyService TDD 1. - Balázs

---
+++ carrental_web/src/test/java/carrental/service/LoyaltyPointServiceTDD.java
@@ -0,0 +1,20 @@
+package carrental.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+@AutoConfigureTestDatabase
+public class LoyaltyPointServiceTDD {
+
+    @Test
+    public void testLoyaltyPointService() {
+
+    }
+}
