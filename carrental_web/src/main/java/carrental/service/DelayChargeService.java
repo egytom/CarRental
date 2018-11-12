a5213cc9f3fa2efa4a8d821259d3c96958d22f80
Boti97 <kurdi.boti@gmail.com>
2018-11-12 18:35:49 +0100

    DelayChargeService TDD 2.

--- carrental_web/src/main/java/carrental/service/DelayChargeService.java
+++ carrental_web/src/main/java/carrental/service/DelayChargeService.java
@@ -1,9 +1,14 @@
 package carrental.service;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import carrental.model.Car;
+import carrental.model.Booking;
 import carrental.model.Client;
 import carrental.repository.BookingRepository;
 import carrental.repository.CarRepository;
@@ -26,7 +31,19 @@ public class DelayChargeService {
 	PaymentRepository paymentRepository;
 	
 	public void delayCharge() {
+		List<Client> clients = clientRepository.findAll();
+		Date today = Calendar.getInstance().getTime();
 		
+		for(Client c : clients) {
+			for(Booking b : c.getBooking()) {
+				if(today.compareTo(b.getToDate()) < 0) {
+					long diffInMillies = Math.abs(today.getTime() - b.getToDate().getTime());
+				    long diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+				    
+				    b.getPrice().setAmount((int) (b.getPrice().getAmount()+(diff*200)));
+				}
+			}
+		}
 	}
 	
 }
